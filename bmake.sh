d764e97c4504f420f2425d56481c52ab05c0fd42
Code-Bee <kevin.zheng@gmail.com>
2013-07-27 19:14:31 +0800

    linux version 6, reorganize to archive

--- bmake.sh
+++ bmake.sh
@@ -1,4 +1,40 @@
 #!/bin/sh
 
-gcc -g MBase/base_struct/*.c MBase/error/*.c normalize_engine/ne/*.c -o ne -IM_include -D__M_CFG_OS64 -D__M_CFG_OS_LINUX -D_DEBUG -Wall 
+# p1: version of normalize engine, x.x.x
+# p2: release/debug
+
+p1=$1;
+p2=$2;
+
+if [ -z "$p1" ] ; then
+    echo "version x.x.x is needed!"
+    echo "format: ./bmake.sh x.x.x debug/release"
+    exit
+fi;
+
+debug_flag="-D_DEBUG -D_DEBUG_PRINT -IM_include -D__M_CFG_OS64 -D__M_CFG_OS_LINUX -Wall -g";
+release_flag="-IM_include -D__M_CFG_OS64 -D__M_CFG_OS_LINUX -Wall -O2";
+
+if [ $p2 == "release" ] ; then
+    flag=$release_flag;
+    p1="${p1}.r";
+else
+    flag=$debug_flag;
+    p1="${p1}.d";
+fi;
+
+
+
+gcc -c MBase/base_struct/*.c $flag
+gcc -c MBase/error/*.c $flag
+gcc -c normalize_engine/ne/*.c $flag
+
+mv MBase/base_struct/*.o .
+mv MBase/error/*.o .
+mv normalize_engine/ne/*.o .
+
+ar -q libne.$p1.a *.o
+rm -f *.o
+
+gcc  normalize_engine/ne_test/*.c -o ne -lne.$p1 $flag -L.
 
